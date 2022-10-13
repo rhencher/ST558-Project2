25c22ce34c156b313736da078eaf5842de7079bd
Sneha Karanjai <sneha4ria@gmail.com>
2022-10-12 23:34:52 -0400

    Update README_Generator.R

--- README_Generator.R
+++ README_Generator.R
@@ -1,3 +1,3 @@
 #Code to render the RMarkdown file and create the README.md file
 
-rmarkdown::render("ST 558 Project 2.Rmd", output_file = "README.md",  params=list(state='NC', ordering='desc', rows = 100, year = 'latest', ownership = 'private', locale = 'town'))
+rmarkdown::render("ST 558 Project 2.Rmd", output_file = "README.md",  params=list(state='NC', ordering='desc', rows = 100, year = 'latest', locale = 'town'))
